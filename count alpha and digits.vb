161f07d3e7a9f9cba9421e164e055bf0db8f7c02
yumnashahid <55451341+yumnashahid@users.noreply.github.com>
2019-10-29 11:51:02 -0700

    Add files via upload

---
+++ count alpha and digits.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim nextChar As Char
+        Dim digit, alpha, count As Integer
+
+        str1 = ""
+        nextChar = ""
+        digit = 0
+        alpha = 0
+        count = 0
+
+        Console.Write("Enter string 1: ")
+        str1 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            nextChar = Mid(str1, count, 1)
+            If nextChar >= "0" And nextChar <= "9" Then
+                digit = digit + 1
+            Else
+                alpha = alpha + 1
+            End If
+        Next
+
+        Console.WriteLine("Digit = " & digit)
+        Console.WriteLine("alpha = " & alpha)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ finding and replacing of a char.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2 As String
+        Dim chr1, chr2, nextChar As Char
+        Dim counter As Integer
+
+        str1 = ""
+        str2 = ""
+        chr1 = ""
+        chr2 = ""
+        nextChar = ""
+        counter = 0
+
+        Console.Write("Enput string 1: ")
+        str1 = Console.ReadLine
+
+        Console.Write("Enter character to replace: ")
+        chr1 = Console.ReadLine
+
+        Console.Write("Enter character to replace with: ")
+        chr2 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            nextChar = Mid(str1, count, 1)
+            If nextChar = chr1 Then
+                str2 = str2 & chr2
+            Else
+                str2 = str2 & nextChar
+            End If
+        Next
+
+        Console.WriteLine(str2)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
