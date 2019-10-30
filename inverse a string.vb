d5361b422ee0761ce0530f0fb844102b86a7ed22
yumnashahid <55451341+yumnashahid@users.noreply.github.com>
2019-10-30 02:20:22 -0700

    Add files via upload

---
+++ inverse a string.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2 As String
+        Dim nextChar As Char
+        Dim count As Integer
+
+        str1 = ""
+        str2 = ""
+        nextChar = ""
+        count = 0
+
+        Console.Write("Enter string 1= ")
+        str1 = Console.ReadLine
+
+        For count = Len(str1) To 1 Step -1
+            nextChar = Mid(str1, count, 1)
+            str2 = str2 & nextChar
+        Next
+
+        Console.WriteLine("str2 = " & str2)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
